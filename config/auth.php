d0afeea1e859caa9c8b69ac95c22b1137830954c
Anthony Walter <maestroanth@gmail.com>
2017-08-12 23:55:01 -0600

    last bit of passport config

--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => Namespace\User::class,
+            'model' => App\User::class,
         ],
 
         // 'users' => [
