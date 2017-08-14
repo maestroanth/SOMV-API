95619900e53d043f9939fab07b89af46d3a3cb52
Anthony Walter <maestroanth@gmail.com>
2017-08-14 13:53:38 -0600

    passport again

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,14 +15,14 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        //$response = $next($request);
+        $response = $next($request);
 
        // $response->header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, PATCH, DELETE');
         //$response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
         //$response->header('Access-Control-Allow-Origin', '*');
         //return $response;
 
-        //return $response;
+        return $response;
 
     }
 }
