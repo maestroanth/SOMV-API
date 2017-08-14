e6a551ce2caea27ae3fed92d87e0c4d9c7ae3268
Anthony Walter <maestroanth@gmail.com>
2017-08-14 15:51:32 -0600

    passport again

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,9 +17,9 @@ class Cors
     {
         $response = $next($request);
 
-        $response->header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, PATCH, DELETE');
-        $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
-        $response->header('Access-Control-Allow-Origin', '*');
+        //$response->header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, PATCH, DELETE');
+        //$response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
+        //$response->header('Access-Control-Allow-Origin', '*');
         //return $response;
 
         return $response;
