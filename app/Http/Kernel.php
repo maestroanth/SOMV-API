13c2a3646e0aa3c5759d42c2f5b1cbf1ec543d37
Anthony Walter <maestroanth@gmail.com>
2017-08-15 10:42:56 -0600

    getting an implicit grant to work

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,8 @@
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use Laravel\Passport\Http\Middleware\CheckClientCredentials;
+
 
 class Kernel extends HttpKernel
 {
@@ -60,5 +62,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'client' => CheckClientCredentials::class,
     ];
 }
