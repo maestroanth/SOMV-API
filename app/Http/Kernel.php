fff97174861cf5ec6d521f23b4b5a8a0b60ee472
Anthony Walter <maestroanth@gmail.com>
2017-08-14 22:33:45 -0600

    trying to get rout to authenticate client secret then make user then spit out access token

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,6 +60,5 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'auth_client' => \Laravel\Passport\Http\Middleware\CheckClientCredentials::class,
     ];
 }
