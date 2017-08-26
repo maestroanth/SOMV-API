64f54a43dd8965c60806f940483d6bb1ed0fcac9
Anthony Walter <maestroanth@gmail.com>
2017-08-26 13:36:10 -0600

    Adding createracestable schema

--- database/migrations/2017_08_26_193425_create_races_table.php
+++ database/migrations/2017_08_26_193425_create_races_table.php
@@ -13,7 +13,25 @@ class CreateRacesTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('races', function (Blueprint $table) {
+            $table->string('id', 100)->primary();
+            $table->string('birth_universe', 100)->foreign();
+            $table->integer('base_intuition');
+            $table->integer('base_experimentation');
+            $table->integer('base_intelligence');
+            $table->integer('base_imagination');
+            $table->integer('base_sanity');
+            $table->integer('base_power_rate');
+            $table->text('race_name');
+            $table->text('racial_bonuses')->nullable();
+            $table->text('description')->nullable();
+            $table->text('personality')->nullable();
+            $table->boolean('is_locked');
+            $table->boolean('is_metaphysical');
+            $table->text('image_1');
+            $table->text('image_2');
+            $table->text('image_3');
+        });
     }
 
     /**
@@ -23,6 +41,6 @@ class CreateRacesTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('races');
     }
 }
