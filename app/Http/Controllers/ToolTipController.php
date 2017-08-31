76d97662a8bf662c05da2443ec441044eef850ed
Anthony Walter <maestroanth@gmail.com>
2017-08-30 23:36:12 -0600

    added another tooltip route

--- app/Http/Controllers/ToolTipController.php
+++ app/Http/Controllers/ToolTipController.php
@@ -25,6 +25,7 @@ class ToolTipController extends Controller
 
     public function getToolTipData($category)
     {
+        $category = parse_int($category);
         $this->response = Tooltip::where('category', $category);
        return $this->response;
     }
