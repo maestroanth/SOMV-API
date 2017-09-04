fe7392703147e8bed0b58e4b43de3d1c51820c8b
Anthony Walter <maestroanth@gmail.com>
2017-09-03 21:56:26 -0600

    Forgot universe card table

--- database/migrations/2017_09_04_034252_creat_universe_cards_table.php
+++ database/migrations/2017_09_04_034252_creat_universe_cards_table.php
@@ -13,7 +13,38 @@ class CreatUniverseCardsTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('universe_cards', function (Blueprint $table) {
+            $table->string('id', 100)->primary();
+            $table->string('name', 100);
+            $table->string('description', 100)->nullable();
+            $table->string('FK_userID', 100);
+            $table->string('FK_base_universe', 100);
+            $table->string('Force_Name_1')->nullable();
+            $table->string('Strength_Force_1');
+            $table->string('Force_Name_2')->nullable();
+            $table->string('Strength_Force_2');
+            $table->string('Force_Name_3')->nullable();
+            $table->string('Strength_Force_3');
+            $table->string('Force_Name_4')->nullable();
+            $table->string('Strength_Force_4');
+
+            $table->string('Concept_Name_1')->nullable();
+            $table->string('Concept_Str_1');
+            $table->string('Concept_Name_2')->nullable();
+            $table->string('Concept_Str_2');
+            $table->string('Concept_Name_3')->nullable();
+            $table->string('Concept_Str_3');
+            $table->string('Concept_Name_4')->nullable();
+            $table->string('Concept_Str_4');
+
+            $table->string('FK_Move_1');
+            $table->string('FK_Move_2');
+            $table->string('FK_Move_3');
+            $table->string('FK_Move_4');
+            $table->string('FK_Move_Ultimate');
+
+
+        });
     }
 
     /**
@@ -23,6 +54,6 @@ class CreatUniverseCardsTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('universes');
     }
 }
