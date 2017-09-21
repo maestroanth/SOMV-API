d5863a1bd96987a97ca1429cbc50d3f9e521b468
Anthony Walter <maestroanth@gmail.com>
2017-09-21 17:12:25 -0600

    adding card destroy route and function

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -127,7 +127,7 @@ class CardController extends Controller
             return $this->response->errorNotFound('User Not Found');
         } else {
             $cards_to_delete = json_decode($request, true);
-            $this->response = $cards_to_delete;
+            $this->response = json_encode($cards_to_delete);
             /*
             $ids_to_delete = array_map(function ($item) {
                 return $item['id'];
