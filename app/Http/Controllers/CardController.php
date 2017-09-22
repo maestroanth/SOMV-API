9ce2542c82dabd0944864711b151179a9aeb0443
Anthony Walter <maestroanth@gmail.com>
2017-09-21 19:43:25 -0600

    fixing bugs

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -127,9 +127,20 @@ class CardController extends Controller
             return $this->response->errorNotFound('User Not Found');
         } else {
             $cards_to_delete = json_decode($request->getContent(), true);
-            $this->response = $cards_to_delete[2];
 
-         
+            for ($i = 0; $i < count($ids_to_delete) - 1; $i++) {
+                $card = Card::find('id', $ids_to_delete[$i])->get();//$i might throw error here
+                $totalEnergy = $totalEnergy + $card->Energy_Value;
+                //calculate all energy of the cards
+            }
+
+            if (DB::table('universe_cards')->whereIn('id', $ids_to_delete)->delete()) {
+
+                $userAccount->Energy = $userAccount->Energy + ($totalEnergy * .2);
+                $this->response = "Universes Destroyed. Refunded Energy: " + ($totalEnergy * .2);
+                //refund user ID $totalEnergy * .2
+
+            }
 
 
 
