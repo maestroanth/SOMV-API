c98d3fe986e0bfbd604eeae216629cf7a9df11f6
Anthony Walter <maestroanth@gmail.com>
2017-11-19 20:44:35 -0700

    blah

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -126,7 +126,7 @@ class CardController extends Controller
          */
         //Get the user
         $userAccount = User::find($id);
-        $card = new Card;//convert it to a 'collection' instead of an array to rid the
+        $card[] = new Card;//convert it to a 'collection' instead of an array to rid the
 
         $totalEnergy = 0;
 
@@ -139,7 +139,7 @@ class CardController extends Controller
 
             for ($i = 0; $i < count($cards_to_delete) - 1; $i++) {
                 //1. It is iterating correctly
-                $this->$card = Card::where('id', $cards_to_delete[$i]['id']);//$i might throw error here
+                $this->$card[$i] = Card::where('id', $cards_to_delete[$i]['id']);//$i might throw error here
 
                 //$totalEnergy = $totalEnergy + $card->Energy_Value;
                 //calculate all energy of the cards
@@ -157,7 +157,7 @@ class CardController extends Controller
 
 
         }
-        $this->response = $cards_to_delete[1]['id']; //json_encode($card);
+        $this->response = json_encode($card[]); //json_encode($card);
         return $this->response;
     }
 }
