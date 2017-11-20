37c27ab9fdf2b6491dae3276dc10e57427dfb0e2
Anthony Walter <maestroanth@gmail.com>
2017-11-19 21:04:36 -0700

    blah

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -157,7 +157,7 @@ class CardController extends Controller
 
 
         }
-        $this->response = json_encode($this->$card); //json_encode($card);
+        $this->response = json_encode($this->$card[0]['Energy_Value']); //json_encode($card);
         return $this->response;
     }
 }
