d75b082b7dbdea2dd8e0a206754668bd72d44b4d
Anthony Walter <maestroanth@gmail.com>
2017-11-19 21:58:09 -0700

    blah

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -162,7 +162,7 @@ class CardController extends Controller
 
             if ($success == true) {
                 $userAccount->Energy = $userAccount->Energy + ($totalEnergy * .2);
-                $this->response = 'Universes Destroyed. Refunded Energy: ' + ($totalEnergy * .2);
+                $this->response = 'Universes Destroyed. Refunded Energy: ' + ($totalEnergy * .2) + ' to UserID: ' + $id;
                 //refund user ID $totalEnergy * .2
             }else{
                  $this->response->errorInternalError('Could not delete Universe(s)');
