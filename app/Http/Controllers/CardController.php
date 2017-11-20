08d90abf2141b5cf779bcdd1628af94181f70d4e
Anthony Walter <maestroanth@gmail.com>
2017-11-19 20:46:11 -0700

    blah

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -157,7 +157,7 @@ class CardController extends Controller
 
 
         }
-        $this->response = json_encode($card); //json_encode($card);
+        $this->response = json_encode(Card::where('id', $cards_to_delete[$i]['id'])); //json_encode($card);
         return $this->response;
     }
 }
