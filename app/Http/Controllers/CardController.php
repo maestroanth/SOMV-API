b6e05e48a45ebefdf1499990cbbe018b5fa6e174
Anthony Walter <maestroanth@gmail.com>
2017-12-30 21:01:44 -0700

    trying to fix it to delete a single card

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -191,10 +191,10 @@ class CardController extends Controller
             if ($success == true) {
 
                 $userAccount['Energy'] = $userAccount['Energy'] + ($totalEnergy * .2);
-                round((float)$userAccount['Energy'], 0);
+                round((float)$userAccount['Energy'], -1);
                 if($userAccount->save()) {
                     $energyRefunded= ($totalEnergy * .2);
-                    round((float)$energyRefunded, 0);
+                    round((float)$energyRefunded, -1);
                     $this->response->setStatusCode(200);
                     $this->response = $energyRefunded;//**I want to return messages, but the front-end doesn't detect as a 'success' when I do and keeps sending requests...odd
 
